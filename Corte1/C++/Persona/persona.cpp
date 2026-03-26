5ca758731410178934e3a3493859470b3652ce5e
Santiagon92 <135183840+Santiagon92@users.noreply.github.com>
2026-03-26 05:30:33 -0500

    Implement Persona class with display functionality
    Added a Persona class with attributes and a method to display information.

--- Corte1/C++/Persona/persona.cpp
+++ Corte1/C++/Persona/persona.cpp
@@ -1,1 +1,33 @@
-a
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Persona{
+public:
+string nombre;
+int edad;
+
+void mostrar(){
+cout<<"Nombre="<< nombre<<"\n";
+cout<<"Edad="<< edad<<"\n";
+}
+};
+
+int main(){
+Persona p1;
+Persona p2;
+
+p1.nombre="Ana";
+p1.edad= 20;
+
+p2.nombre="Luis";
+p2.edad= 22;
+
+cout<<"Objeto 1:\n";
+p1.mostrar();
+
+cout<<"\n Objeto 2:";
+p2.mostrar();
+
+return 0;
+}
